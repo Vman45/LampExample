6ec01bf8f0330744c373d971340d78fbb74fcfd1
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 16:48:34 -0700

    inserts into tbale

--- index.php
+++ index.php
@@ -2,13 +2,26 @@
 $servername = "localhost";
 $username = "test";
 $password = "password";
+$dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password);
+$conn = new mysqli($servername, $username, $password, $dbname);
 
 // Check connection
 if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 echo "Connected successfully to database!";
+
+$sql = "INSERT INTO users (id, name, email)
+VALUES ('id', 'name', 'test@example.com')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
 ?>
