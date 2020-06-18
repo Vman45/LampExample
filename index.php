e00fa8aa4ae214a7f944de4d89c5e43e9f9c8622
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 15:20:29 -0700

    added form example

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
+<html>
+<body>
+
+<form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
+  Name: <input type="text" name="fname">
+  <input type="submit">
+</form>
+
 <?php
-echo "hello world";
-echo date('Y-m-d H:i:s');
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $name = $_REQUEST['fname'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    echo $name;
+  }
+}
 ?>
+
+</body>
+</html>
