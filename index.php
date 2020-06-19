3ccf852246fbb5e67696c366ba614ed4d7797d42
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 17:13:34 -0700

    added display for all users

--- index.php
+++ index.php
@@ -13,14 +13,26 @@ if ($conn->connect_error) {
 }
 echo "Connected successfully to database!";
 
-$sql = "INSERT INTO users (id, name, email)
-VALUES ('id', 'name', 'test@example.com')";
+// $sql = "INSERT INTO users (id, name, email) VALUES ('id', 'name', 'test@example.com')";
+//
+// if ($conn->query($sql) === TRUE) {
+//   echo "New record created successfully";
+// } else {
+//   echo "Error: " . $sql . "<br>" . $conn->error;
+// }
 
-if ($conn->query($sql) === TRUE) {
-  echo "New record created successfully";
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
+$sql = "SELECT * FROM MY_TABLE";
+$result = mysqli_query($conn, $sql); // First parameter is just return of "mysqli_connect()" function
+echo "<br>";
+echo "<table border='1'>";
+while ($row = mysqli_fetch_assoc($result)) { // Important line !!! Check summary get row on array ..
+    echo "<tr>";
+    foreach ($row as $field => $value) { // I you want you can right this line like this: foreach($row as $value) {
+        echo "<td>" . $value . "</td>"; // I just did not use "htmlspecialchars()" function.
+    }
+    echo "</tr>";
 }
+echo "</table>";
 
 $conn->close();
 
