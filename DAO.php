0799b7bfc21f652d8789e72683dcfc34b1f68a5e
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 18:03:10 -0700

    Possibly fixed code

--- DAO.php
+++ DAO.php
@@ -1,10 +1,10 @@
 <?php
 class DAO {
-  public $servername;
-  public $username;
-  public $password;
-  public $dbname;
-  public $conn;
+  private $servername;
+  private $username;
+  private $password;
+  private $dbname;
+  private $conn;
 
   public function __construct()
   {
@@ -12,13 +12,12 @@ class DAO {
     $this->username = "test";
     $this->password = "password";
     $this->dbname = "test";
+    $this->conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
   }
 
   function displayUsers() {
-    $conn = new mysqli($this->servername, $this->username, $this->password, $this->dbname);
-
     $sql = "SELECT * FROM users";
-    $result = mysqli_query($conn, $sql);
+    $result = mysqli_query($this->conn, $sql);
     echo "<br>";
     echo "<table border='1'>";
     while ($row = mysqli_fetch_assoc($result)) {
