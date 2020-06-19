1c9b89e9a864fd4b232346368f118bb47b6f07e6
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 17:51:08 -0700

    Possibly fixed code

--- DAO.php
+++ DAO.php
@@ -33,7 +33,7 @@
 // $dao = new DAO()
 // $dao.displayUsers()
 
-class Fruit {
+class DAO {
   // Properties
   public $name;
   public $color;
@@ -41,14 +41,15 @@ class Fruit {
   // Methods
   function set_name($name) {
     $this->name = $name;
+    echo $name;
   }
   function get_name() {
     return $this->name;
   }
 }
 
-$apple = new Fruit();
-$banana = new Fruit();
+$apple = new DAO();
+$banana = new DAO();
 $apple->set_name('Apple');
 $banana->set_name('Banana');
 
