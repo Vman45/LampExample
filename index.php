b535f1556cb2a64f4f80185b7a5aa069c91119e3
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 21:07:38 -0700

    added webpage title

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <html>
+
   <head>
+
     <title>LAMP Stack</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
   </head>
+
   <body>
 
-    <h1>LAMP Stack Example</h1>
-
     <div class="container">
+
+      <h1>LAMP Stack Example</h1>
 
       <h2>Create User</h2>
 
@@ -64,4 +67,5 @@
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
   </body>
+
 </html>
