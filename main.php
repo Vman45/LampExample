1e53dab36cd3cf84bde3e03fe8d3f2d3db33b6c8
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 20:39:20 -0700

    removed connection status

--- main.php
+++ main.php
@@ -3,9 +3,9 @@ require 'DAO.php';
 $dao = new DAO();
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $name = $_POST['fname'];
-  $email = $_POST['femail'];
-  $id = $_POST['fid'];
+  $name = trim($_POST['fname']);
+  $email = trim($_POST['femail']);
+  $id = trim($_POST['fid']);
 
   if(!empty($name) && !empty($email)) {
     $dao->createUser($name, $email);
