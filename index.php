4d86a2efec2c13b5d706eb2a0881a28f1f218bfc
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 21:05:32 -0700

    added webpage title

--- index.php
+++ index.php
@@ -10,9 +10,11 @@
     <h1>LAMP Stack Example</h1>
 
     <div class="container">
+
       <h2>Create User</h2>
 
       <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
         <div class="form-group row">
           <label for="inputName" class="col-sm-2 col-form-label">Name</label>
           <div class="col-sm-10">
@@ -30,22 +32,32 @@
         <div class="form-group row">
           <div class="offset-sm-2 col-sm-10">
             <input type="submit" value="Create User" class="btn btn-primary"/>
-
           </div>
         </div>
 
       </form>
+
+      <h2>Delete User</h2>
+
+      <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+
+        <div class="form-group row">
+          <label for="inputID" class="col-sm-2 col-form-label">ID</label>
+          <div class="col-sm-10">
+            <input class="form-control" id="inputID" type="text" name="fid">
+          </div>
+        </div>
+
+        <div class="form-group row">
+          <div class="offset-sm-2 col-sm-10">
+            <input type="submit" value="Delete User" class="btn btn-primary">
+          </div>
+        </div>
+
+      </form>
+
+      <?php require 'main.php';?>
     </div>
-
-
-    <h2>Delete User</h2>
-
-    <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      ID: <input type="text" name="fid">
-      <input type="submit" value="Delete User">
-    </form>
-
-    <?php require 'main.php';?>
 
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
