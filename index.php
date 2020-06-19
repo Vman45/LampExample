8866b7f3de44cc6204463a7388c7935bf1b412a2
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 20:57:47 -0700

    added webpage title

--- index.php
+++ index.php
@@ -6,13 +6,17 @@
 
   </head>
   <body>
-    
+
     <h1>LAMP Stack Example</h1>
 
     <h2>Create User</h2>
 
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
-      Name: <input type="text" name="fname">
+      <div class="form-group row">
+        <label for="inputName" class="col-sm-2 col-form-label">Name</label>
+        <input class="form-control" id="inputName" placeholder="Name" type="text" name="fname">
+      </div>
+
       Email: <input type="text" name="femail">
       <input type="submit" value="Create User">
     </form>
