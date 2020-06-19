85731812d186c7d803e11c8b546bda7e468190e7
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 17:14:48 -0700

    updated code

--- index.php
+++ index.php
@@ -2,10 +2,9 @@
 $servername = "localhost";
 $username = "test";
 $password = "password";
-$dbname = "test";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password);
 
 // Check connection
 if ($conn->connect_error) {
