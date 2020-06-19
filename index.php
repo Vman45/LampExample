77df8d5383ba49c0424be876fceec2db33f7d047
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 20:16:05 -0700

    added label to table

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
   <body>
     <h1>LAMP Stack Example</h1>
 
+    <?php require 'connectionStatus.php';?>
+
 
     <h2>Create User</h2>
 
@@ -19,6 +21,8 @@
     </form>
 
     <?php require 'table.php';?>
+
+
 
 
   </body>
