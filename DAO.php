8affb41c6d91463ff44c67bdc875a20bcb7439f4
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 17:46:32 -0700

    Possibly fixed code

--- DAO.php
+++ DAO.php
@@ -1,14 +1,22 @@
 <?php
 class DAO {
-  const SERVERNAME = "localhost";
-  const USERNAME = "test";
-  const PASSWORD = "password";
-  const DBNAME = "test";
-  const CONNECTION = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
+  // const SERVERNAME = "localhost";
+  // const USERNAME = "test";
+  // const PASSWORD = "password";
+  // const DBNAME = "test";
+  // const CONNECTION = new mysqli(SERVERNAME, USERNAME, PASSWORD, DBNAME);
 
   function displayUsers() {
+    $servername = "localhost";
+    $username = "test";
+    $password = "password";
+    $dbname = "test";
+
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+
     $sql = "SELECT * FROM users";
-    $result = mysqli_query(CONNECTION, $sql);
+    $result = mysqli_query($conn, $sql);
     echo "<br>";
     echo "<table border='1'>";
     while ($row = mysqli_fetch_assoc($result)) {
