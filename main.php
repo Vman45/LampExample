4bc779542a6a4460466c4384e5825409dd94847e
rdeepak2002 <rdeepak2002@gmail.com>
2020-06-18 19:59:45 -0700

    added delete system

--- main.php
+++ main.php
@@ -12,7 +12,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
       echo "Fields cannot be empty!";
     }
     else  {
-      echo "delete user";
+      $dao->deleteUser($id);
     }
   }
   else {
